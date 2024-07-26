90839f8c7b99dd9edcbdf1a56855487f3484a481
xonack <jenshonack@gmail.com>
2024-07-26 19:02:32 +0100

    first commit

---
+++ app/api/getApiKey/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import { Rettiwt } from 'rettiwt-api';
+
+const rettiwt = new Rettiwt();
+
+export async function POST(request: Request) {
+  const { email, username, password } = await request.json();
+  
+  try {
+    const apiKey: string = await rettiwt.auth.login(email, username, password);
+    return NextResponse.json({ apiKey });
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to get API key' }, { status: 500 });
+  }
+}
